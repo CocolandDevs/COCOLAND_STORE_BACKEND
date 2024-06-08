67fd6063402f91f2da38329485ace14792481a10
Noe Leonel Rosado Quintal <leonelrosado2407@gmail.com>
2024-06-07 18:02:39 -0600

    chore: Add login, logout, and token verification functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import prisma from '../libs/client.js';
 import { createAccessToken } from '../libs/jwt.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+
+const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY;
 
 export const getUsers = async () => {
     const users = await prisma.uSUARIOS.findMany();
@@ -35,5 +38,61 @@ export const register = async (req,res) => {
     } catch (error) {
         res.status(500).json([error.message]);
     }
+
+}
+
+export const login = async (req,res) => {
+    try {
+        const { email, password } = req.body;
+        
+        const userfound = await prisma.uSUARIOS.findUnique({ where: { email } });
+        
+        if (!userfound) return res.status(400).json(["User not found"]);
+
+        const isMatch = await bcrypt.compare(password, userfound.password);
+
+        if (!isMatch) return res.status(400).json(["Invalid credentials"]);
+
+        const token = await createAccessToken({ id: userfound.id });
+
+        res.cookie("token", token);
+
+        res.status(200).json({
+            message: "User logged in successfully",
+            user: userfound
+        });
+
+    } catch (error) {
+        res.status(500).json([error.message]);
+    }
+}
+
+export const logout = (req,res) =>{
+    res.cookie('access_token',"",{
+     expires : new Date(0)
+    });
+ 
+    return res.status(200).json({message : "User logged out"});
+}
+
+export const veifyToken = async (req,res) =>{
+    const {access_token} = req.cookies;
+
+    if (!access_token) return res.status(401).json(["Unauthorized"]);
+    
+    jwt.verify(access_token,TOKEN_SECRET_KEY,async (err,user)=>{
+
+        if(err) return res.status(401).json(["Unauthorized"]);
+        
+        const userFound  = await prisma.uSUARIOS.findUnique({where : {id : user.id}});
+
+        if (!userFound) return res.status(400).json( ["User not found"]);
+
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+        });
+    })
 
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { getUsers, register } from "../controllers/auth.controller.js";
+import { login, register, veifyToken } from "../controllers/auth.controller.js";
 import { validateschema } from "../middlewares/validator.middleware.js";
-import { registerSchema } from "../schemas/auth.schema.js";
+import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
+import { authRequired } from "../middlewares/validateToken.js";
 
 
 const router = Router();
 //para crear la ruta primero ponemos el nombre de laruta + si es que validaremos algo + la función que se ejecutará
 
 router.post("/register", validateschema(registerSchema) ,register);
+router.post("/login",validateschema(loginSchema) , login);
+router.get("/auth/verify", authRequired , veifyToken );
 
 export default router;
